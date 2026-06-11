4e5c29fb2a91c6c17bbb1d399ead2d9fe09d1cb4
Andrew Rodriguez <aroday23@gmail.com>
2026-06-11 13:38:19 -0500

    Fix bug

--- test-commands.sh
+++ test-commands.sh
@@ -21,19 +21,19 @@ cd "benchmarks"
 nvcc -O3 -Xcompiler -fopenmp -arch=$GPU_ARCH -DBITS_FOR_EXPONENT_32=8 -DBITS_FOR_MANTISSA_32=23 -DUVM1 "$CODE_DIR/$NATIVE_SRC" -o native
 nsys profile --stats=true --cuda-um-gpu-page-faults=true --cuda-um-cpu-page-faults=true --show-output=true ./native
 
-nvcc -O3 -Xcompiler -fopenmp -arch=$GPU_ARCH -DBITS_FOR_EXPONENT_32=8 -DBITS_FOR_MANTISSA_32=23 -DUVM1 "$CODE_DIR/$NATIVE_SRC" -o fitflt
+nvcc -O3 -Xcompiler -fopenmp -arch=$GPU_ARCH -DBITS_FOR_EXPONENT_32=8 -DBITS_FOR_MANTISSA_32=23 -DUVM1 "$CODE_DIR/$FITFLT_SRC" -o fitflt
 nsys profile --stats=true --cuda-um-gpu-page-faults=true --cuda-um-cpu-page-faults=true --show-output=true ./fitflt
 
-nvcc -O3 -Xcompiler -fopenmp -arch=$GPU_ARCH -DBITS_FOR_EXPONENT_32=8 -DBITS_FOR_MANTISSA_32=19 -DUVM1 "$CODE_DIR/$NATIVE_SRC" -o fitflt
+nvcc -O3 -Xcompiler -fopenmp -arch=$GPU_ARCH -DBITS_FOR_EXPONENT_32=8 -DBITS_FOR_MANTISSA_32=19 -DUVM1 "$CODE_DIR/$FITFLT_SRC" -o fitflt
 nsys profile --stats=true --cuda-um-gpu-page-faults=true --cuda-um-cpu-page-faults=true --show-output=true ./fitflt
 
-nvcc -O3 -Xcompiler -fopenmp -arch=$GPU_ARCH -DBITS_FOR_EXPONENT_32=8 -DBITS_FOR_MANTISSA_32=15 -DUVM1 "$CODE_DIR/$NATIVE_SRC" -o fitflt
+nvcc -O3 -Xcompiler -fopenmp -arch=$GPU_ARCH -DBITS_FOR_EXPONENT_32=8 -DBITS_FOR_MANTISSA_32=15 -DUVM1 "$CODE_DIR/$FITFLT_SRC" -o fitflt
 nsys profile --stats=true --cuda-um-gpu-page-faults=true --cuda-um-cpu-page-faults=true --show-output=true ./fitflt
 
-nvcc -O3 -Xcompiler -fopenmp -arch=$GPU_ARCH -DBITS_FOR_EXPONENT_32=8 -DBITS_FOR_MANTISSA_32=11 -DUVM1 "$CODE_DIR/$NATIVE_SRC" -o fitflt
+nvcc -O3 -Xcompiler -fopenmp -arch=$GPU_ARCH -DBITS_FOR_EXPONENT_32=8 -DBITS_FOR_MANTISSA_32=11 -DUVM1 "$CODE_DIR/$FITFLT_SRC" -o fitflt
 nsys profile --stats=true --cuda-um-gpu-page-faults=true --cuda-um-cpu-page-faults=true --show-output=true ./fitflt
 
-nvcc -O3 -Xcompiler -fopenmp -arch=$GPU_ARCH -DBITS_FOR_EXPONENT_32=4 -DBITS_FOR_MANTISSA_32=12 -DUVM1 "$CODE_DIR/$NATIVE_SRC" -o fitflt
+nvcc -O3 -Xcompiler -fopenmp -arch=$GPU_ARCH -DBITS_FOR_EXPONENT_32=4 -DBITS_FOR_MANTISSA_32=12 -DUVM1 "$CODE_DIR/$FITFLT_SRC" -o fitflt
 nsys profile --stats=true --cuda-um-gpu-page-faults=true --cuda-um-cpu-page-faults=true --show-output=true ./fitflt
 
 rm -f report*
